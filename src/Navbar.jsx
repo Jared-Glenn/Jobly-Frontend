37b325bb542e0feb024cc230c05336662ea30f0c
Jared <jared_glenn@yahoo.com>
2024-05-02 14:03:49 -0600

    add Navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './Navbar.css';
 
 function Navbar() {
     return (
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/companies">Companies</Link>
-            <Link to="/jobs">Jobs</Link>
-            <Link to="/profile">Profile</Link>
+        <div className="navbar">
+            <span className="navbar-button">
+                <Link className="navbar-link" to="/">Home</Link>
+            </span>
+            <span className="navbar-button">
+            <Link className="navbar-link" to="/companies">Companies</Link>
+            </span>
+            <span className="navbar-button">
+            <Link className="navbar-link" to="/jobs">Jobs</Link>
+            </span>
+            <span className="navbar-button">
+            <Link className="navbar-link" to="/profile">Profile</Link>
+            </span>
         </div>
     )
 }
 
 export default Navbar;
+
+//Photo by <a href="https://unsplash.com/@orlovamaria?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Maria Orlova</a> on <a href="https://unsplash.com/photos/a-close-up-view-of-a-blue-water-surface-Q3Ea7QQQ6MA?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
