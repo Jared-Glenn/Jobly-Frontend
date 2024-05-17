a47f4762946b9c7cc4f1f344cb72882adb80b9b5
Jared <jared_glenn@yahoo.com>
2024-05-17 14:36:36 -0600

    start work on SignUpForm

--- src/SignUpForm.jsx
+++ src/SignUpForm.jsx
@@ -1,9 +1,48 @@
 import React from "react";
 
-function SignUpForm() {
+function SignUpForm({ onSubmit }) {
+    const [ formData, setFormData ] = useState({
+        username: "",
+        password: "",
+        firstName: "",
+        lastName: "",
+        email: ""
+    });
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setFormData(data => ({
+            ...data, 
+            [ name ]: value
+        }))
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const { username, password, firstName, lastName, email } = formData;
+        onSubmit( username, password, firstName, lastName, email);
+        setFormData({
+            username: "",
+            password: "",
+            firstName: "",
+            lastName: "",
+            email: ""
+        });
+    }
+
     return (
         <>
-            <h1>SignUpForm H1</h1>
+            <form className="searchForm" onSubmit={ handleSubmit }>
+                <input className="searchBar"
+                    id="searchTerm"
+                    type="text"
+                    name="searchTerm"
+                    placeholder="Enter search term"
+                    value={formData.searchTerm || ""}
+                    onChange={ handleChange }
+                />
+                <button className="searchButton" onClick={ handleSubmit }>Search</button>
+            </form>
         </>
     )
 }
