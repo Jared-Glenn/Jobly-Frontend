f2deb17edbc4abcbc7985fbc79abb074943ea516
Jared <jared_glenn@yahoo.com>
2024-05-18 23:37:26 -0600

    add signup form

--- src/SignUpForm.jsx
+++ src/SignUpForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SignUpForm({ onSubmit }) {
     const [ formData, setFormData ] = useState({
@@ -32,16 +32,44 @@ function SignUpForm({ onSubmit }) {
 
     return (
         <>
-            <form className="searchForm" onSubmit={ handleSubmit }>
-                <input className="searchBar"
-                    id="searchTerm"
+            <form className="signupForm" onSubmit={ handleSubmit }>
+                <label htmlFor="username">Username</label>
+                <input className="username-input"
+                    id="username"
                     type="text"
-                    name="searchTerm"
-                    placeholder="Enter search term"
-                    value={formData.searchTerm || ""}
+                    name="username"
+                    placeholder="Username"
+                    value={formData.username || ""}
                     onChange={ handleChange }
                 />
-                <button className="searchButton" onClick={ handleSubmit }>Search</button>
+                <label htmlFor="password">Password</label>
+                <input className="password-input"
+                    id="password"
+                    type="text"
+                    name="password"
+                    placeholder="Password"
+                    value={formData.password || ""}
+                    onChange={ handleChange }
+                />
+                <label htmlFor="fname">First Name</label>
+                <input className="fname-input"
+                    id="fname"
+                    type="text"
+                    name="fname"
+                    placeholder="First Name"
+                    value={formData.fname || ""}
+                    onChange={ handleChange }
+                />
+                <label htmlFor="lname">Last Name</label>
+                <input className="lname-input"
+                    id="lname"
+                    type="text"
+                    name="lname"
+                    placeholder="Last Name"
+                    value={formData.lname || ""}
+                    onChange={ handleChange }
+                />
+                <button className="signup-form" onClick={ handleSubmit }>Sign Up!</button>
             </form>
         </>
     )
