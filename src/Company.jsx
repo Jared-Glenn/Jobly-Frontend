d57610b8f8946844830d6682a50dfe1a17f0e855
Jared <jared_glenn@yahoo.com>
2024-05-28 18:04:32 -0600

    bug fix

--- src/Company.jsx
+++ src/Company.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Company.css";
 
 function Company({ handle, name, description }) {
     return (
-        <a href={`/companies/${ handle }`}>
+        <Link to={`/companies/${ handle }`}>
             <div className="company">
                 <h2 className="company-name">{ name }</h2>
                 <p>{ description }</p>
             </div>
-        </a>
+        </Link>
     )
 }
 
